ff472ca245990f1e461752f12971f1bafac0d3fa
LucasLiSan <lukassantana_rgt@hotmail.com>
2024-07-01 17:20:31 -0300

    criação do form cadastro de itens teste, estilziação pra responsividade

--- controllers/almoxarifadoController.js
+++ controllers/almoxarifadoController.js
@@ -49,7 +49,50 @@ router.get('/almoxarifado', async function(req, res) {
 /* ----------/\ ROTA PRINCIPAL VIEW ALMOXARIFADO /\---------- */
 
 /* ----------\/ ROTA ADIÇÃO NO DB ALMOXARIFADO \/---------- */
-//router.post('/almoxarifado/add')
+router.post('/almoxarifado/add', async (req,res) => {
+    console.log(req.body);
+    const {
+        nomeProduto,
+        descricaoProduto,
+        corProduto,
+        qtdEstoque,
+        categoriaProd,
+        codBarraProd,
+        armarioaAlmoxarifado,
+        prateleiraAlmoxarifado
+    } = req.body;
+    try {
+        // Cria um novo produto no almoxarifado
+        const novoProduto = await Almoxarifado.create({
+            nomeProduto : nomeProduto,
+            descricaoProduto : descricaoProduto,
+            corProduto : corProduto,
+            qtdEstoque : qtdEstoque,
+            categoriaProd : categoriaProd,
+            codBarraProd : codBarraProd
+        });
+
+        // Cria uma nova localização para o produto
+        await Localizacao.create({
+            armario: armarioaAlmoxarifado,
+            prateleira: prateleiraAlmoxarifado,
+            idProdutoGuardado: novoProduto.idProduto,
+            qtdPrdoGuardado: qtdEstoque,
+            localArmario: 'ALMOXARIFADO'
+        });
+
+        res.redirect("/almoxarifado")
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Erro ao cadastrar produto.");
+    }
+});
 /* ----------/\ ROTA ADIÇÃO NO DB ALMOXARIFADO /\---------- */
+
+/* ----------\/ ROTA UPDATE DB ALMOXARIFADO \/---------- */
+
+
+
+/* ----------/\ ROTA UPDATE DB ALMOXARIFADO /\---------- */
 
 export default router;
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 /* ----------\/ IMPORTAÇÃO DE MÓDULOS \/---------- */
-import express from "express";
-import session from "express-session";
 import connection from "./configuration/sequelizeConfig.js";
+import express from "express";
 import flash from "express-flash";
 import ip from "ip";
+import multer from "multer";
+import session from "express-session";
 /* ----------/\ IMPORTAÇÃO DE MÓDULOS /\---------- */
 const app = express();
+
+/* ----------\/ IMPORTANDO OS CONTROLLERS \/---------- */
+import almoxarifadoController from "./controllers/almoxarifadoController.js"
+import eCozinhaController from "./controllers/eCozinhaController.js"
+import eLimpezaController from "./controllers/eLimpezaController.js"
+import homeController from "./controllers/homeController.js"
+import patrimonioController from "./controllers/patrimonioController.js"
+/* ----------/\ IMPORTANDO OS CONTROLLERS /\---------- */
 
 /* ----------\/ CRIAÇÃO DO BANCO DE DADOS \/---------- */
 connection.authenticate().then(() => { console.log("Conexão com o banco realizada com sucesso!") }).catch((error) => { console.log(error) });
@@ -13,41 +22,34 @@ connection.authenticate().then(() => { console.log("Conexão com o banco realiza
 connection.query(`CREATE DATABASE IF NOT EXISTS lanaSystemDB;`).then(() => { console.log("Banco de dados criado!") }).catch((error) => { console.log(error) });
 /* ----------/\ CRIAÇÃO DO BANCO DE DADOS /\---------- */
 
-/* ----------\/ IMPORTANDO E INICIANDO OS CONTROLLERS \/---------- */
-import almoxarifadoController from "./controllers/almoxarifadoController.js"
-import eCozinhaController from "./controllers/eCozinhaController.js"
-import eLimpezaController from "./controllers/eLimpezaController.js"
-import homeController from "./controllers/homeController.js"
-import patrimonioController from "./controllers/patrimonioController.js"
-import multer from "multer";
-
-app.use("/", almoxarifadoController);
-app.use("/", eCozinhaController);
-app.use("/", eLimpezaController);
-app.use("/", homeController);
-app.use("/", patrimonioController);
-/* ----------/\ IMPORTANDO E INICIANDO OS CONTROLLERS /\---------- */
-
 /* ----------\/ DEFINIÇÕES BÁSICAS \/---------- */
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(flash());
 app.use(session({
     secret: "Secret",
-    cookie: { maxAge: 86400000},
+    cookie: { maxAge: 86400000 },
     saveUninitialized: false,
     resave: false
 }));
 /* ----------/\ DEFINIÇÕES BÁSICAS /\---------- */
 
+/* ----------\/ INICIANDO OS CONTROLLERS \/---------- */
+app.use("/", almoxarifadoController);
+app.use("/", eCozinhaController);
+app.use("/", eLimpezaController);
+app.use("/", homeController);
+app.use("/", patrimonioController);
+/* ----------/\ INICIANDO OS CONTROLLERS /\---------- */
+
 /* ----------\/ INICIANDO SERVIDOR \/---------- */
 const port = 8080;
-const myServer = ip.address();
-console.log(myServer);
+//const myServer = ip.address();
+//console.log(myServer);
 
-app.listen(port, myServer, function(erro){
+app.listen(port, function(erro){
     if(erro){ console.log("Ocorreu um erro!"); }
     else { console.log(`Servidor iniciado com sucesso!`); }
 });
